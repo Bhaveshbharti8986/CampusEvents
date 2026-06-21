5de6d1f2a29629e5d70574761e8bd6a7ba347537
Bhaveshbharti8986 <bhaveshbharti8986@gmail.com>
2026-06-21 14:42:59 +0530

    Update config.js

--- CampusEvents/Backend/src/config/config.js
+++ CampusEvents/Backend/src/config/config.js
@@ -21,7 +21,9 @@ if(!process.env.GOOGLE_REFRESH_TOKEN){
 if(!process.env.GOOGLE_CLIENT){
   throw new Error("Chaeck the .env file GOOGLE_CLIENT error in GOOGLE_CLIENT")
 }
-
+if(!process.evn.RESEND_API_KEY){
+   throw new Error("Chaeck the .env file RESEND_API_KEY error in RESEND_API_KEY")
+}
 const config = {
   MONGO_URI:process.env.MONGO_URI,
   DB_NAME:'school',
@@ -30,6 +32,7 @@ const config = {
   GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
   GOOGLE_CLIENT:process.env.GOOGLE_CLIENT,
-  GOOGLE_REFRESH_TOKEN:process.env.GOOGLE_REFRESH_TOKEN
+  GOOGLE_REFRESH_TOKEN:process.env.GOOGLE_REFRESH_TOKEN,
+  RESEND_API_KEY:process.evn.RESEND_API_KEY
 };
 export default config;
